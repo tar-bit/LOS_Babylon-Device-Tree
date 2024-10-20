0dce8228955762b5a3b6070663de240ea0554bbf
tar-bit <tecnocracy@proton.me>
2024-10-20 18:45:52 -0400

    Update lineage_babylon.mk

--- lineage_babylon.mk
+++ lineage_babylon.mk
@@ -10,6 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_foldable_book_telephony.mk)
 
 # Inherit from generic device
 $(call inherit-product, device/xiaomi/babylon/device.mk)
