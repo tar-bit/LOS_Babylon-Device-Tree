7943ad801dec6945b510280442ce9857f1716212
tar-bit <tecnocracy@proton.me>
2024-10-24 08:23:10 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,8 +17,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
+#    android.hardware.boot@1.2-impl \
+#    android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
